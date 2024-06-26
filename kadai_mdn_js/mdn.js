999fa59daaf6cd3b872915aa61c8ac3fdb30d78b
hachioji159 <ithachioji159@gmail.com>
2024-06-26 14:50:20 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const today = new Date();
+
+
+const todayYear = today.getFullYear();
+const todayMonth = today.getMonth() + 1;
+const todayDay = today.getDate();
+
+console.log(todayYear + "年" + todayMonth + "月" + todayDay + "月");
