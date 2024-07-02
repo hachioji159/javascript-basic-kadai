291bcd16535d327352a92a3f1f998ce4f222b6f9
hachioji159 <ithachioji159@gmail.com>
2024-07-02 11:19:29 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,9 @@
+const changeBtn = document.getElementById('btn');
+
+changeBtn.addEventListener('click', () => {
+  const text = document.getElementById('text');
+
+  setTimeout(() => {
+    text.textContent = 'ボタンをクリックしました';
+  }, 2000);
+});
